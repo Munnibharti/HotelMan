a166a7b8def6f6722267b8757388c4951c845ad8
Munnibharti <umabharti56789@gmail.com>
2024-07-29 02:01:12 +0530

    Merge branch 'featurehotel' of https://github.com/Munnibharti/HotelMan into featurehotel

--- HotelManagementProject/Controllers/StaffController.cs
+++ HotelManagementProject/Controllers/StaffController.cs
@@ -8,5 +8,47 @@ namespace HotelManagementProject.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                await _staffServices.CreateStaffAsync(staff);
+                return RedirectToAction("Index");
+            }
+            return View(staff);
+        }
+
     }
+
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(string id)
+    {
+        var staff = await IStaffService.GetStaffByIdAsync(id);
+        if (staff == null)
+        {
+            return NotFound();
+        }
+        return View(staff);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(string id, [Bind("Id,Staff_Name,Position,Salary")] Staff staffDetails)
+    {
+        if (id != staffDetails.Id)
+        {
+            return BadRequest();
+        }
+
+        if (ModelState.IsValid)
+        {
+            await _guestService.UpdateGuestAsync(id, guestDetails);
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(guestDetails);
+    }
+
 }
